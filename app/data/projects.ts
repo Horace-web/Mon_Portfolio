7107ed6df3940b7ae4310d13ce1af2db5a91a85a
Odounlami Horace <odounlamihorace2006@gmail.com>
2026-02-26 11:55:05 +0100

    version 1.2

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -17,7 +17,7 @@ export const projects: Project[] = [
     id: 2,
     title: "Php-Social-Network",
     description:
-      "Simulation d'un réseau Social avec gestion des utilisateurs, publications et commentaires. Projet non achevé",
+      "Simulation d'un réseau Social avec gestion des utilisateurs, publications et commentaires.",
     year: 2025,
     image: "/projects/7046464.jpg",
     technologies: ["Laravel", "Mysql", "Blade"],
